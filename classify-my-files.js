c53d3e9ac89358a492fcd8e52a74a9c78e1cd73c
MXXXXXS <relife_abyss@qq.com>
2018-07-21 16:30:46 +0800

    v1.1.0
    只需要指定原和目的文件夹即可, 优化代码

--- classify-my-files.js
+++ classify-my-files.js
@@ -1,3 +1,4 @@
+#! /usr/bin/env node
 const fs = require('fs'),
     fsPromises = require('fs').promises,
     exif = require('./get-exif.js'),
@@ -6,69 +7,90 @@ const fs = require('fs'),
 //转换exif时间为Date对象
 const { parse } = require('exif-date')
 //读取配置文件
-let Config = fs.readFileSync('./CMFConfig.json', 'utf8')
-if (!Config) throw 'read CMFConfig.json failed!'
-let CMFConfig = JSON.parse(Config),
-    { source, destination, regex } = CMFConfig
-
-let openedEP = exif.openExif(),//启动exiftool
-    fArr = fr.getFilesArr(source),//获得文件列表
-    pArr = []//存放fArr.forEach产生的promise
-
-fArr.forEach(fPath => {
-    pArr.push(
-        Promise
-            .all([
-                exif.getDate(openedEP, fPath, (metadata) => {
-                    if (metadata.data[0].CreateDate) return parse(metadata.data[0].CreateDate)
-                }),
-                // new Promise(function (res, rej) {
-                //     //正则分析文件名蕴含的日期
-                // }),
-                fsPromises
-                    .stat(fPath)
-                    .then(stats => {
-                        return new Date(
-                            Math.min(...[
-                                stats.birthtime.valueOf(),
-                                stats.mtime.valueOf(),
-                                stats.ctime.valueOf()
-                            ])
-                        )
+const Config = fs.readFileSync('./CMFConfig.json', 'utf8')
+if (!Config) throw 'Reading "CMFConfig.json" failed!\nMaybe there\'s no CMFConfig.json\nPlease create one'
+const { source, destination } = JSON.parse(Config)
+pArr = []//存放fArr.forEach产生的promise
+fArr = fr.getFilesArr(source)//获得文件列表
+console.log('The list of files\'names has been created!\nStart getting each file\'s date...')
+exif
+    .openExif()//启动exiftool
+    .then(() => {
+        fArr.forEach(file => {
+            pArr.push(//将一堆Promise装入数组, 以备后用
+                Promise
+                    .all([//三组日期选出最早的日期
+                        exif
+                            .getDate(file)
+                            .then((metadata) => {
+                                return metadata.data[0].CreateDate ? parse(metadata.data[0].CreateDate) : undefined
+                            })
+                            .catch(() => undefined),//正则分析文件名蕴含的日期
+                        Promise
+                            .resolve()
+                            .then(() => {
+                                let regex = new RegExp(/(\d{4})-([01]\d)-([0123]\d)|(\d{4})([01]\d)([0123]\d)/),//支持类似: 2018-07-21, 20180721
+                                    arr = path.basename(file).match(regex)
+                                if (arr) {
+                                    let s = new Set(arr)
+                                    s.delete(undefined)
+                                    let a = [...s]
+                                    a.shift()
+                                    return new Date(a[0], a[1] - 1, a[2]) ? new Date(a[0], a[1] - 1, a[2]) : undefined
+                                }
+                            })
+                            .catch(() => undefined),
+                        fsPromises
+                            .stat(file)
+                            .then(stats => {
+                                return new Date(
+                                    Math.min(...[
+                                        stats.birthtime.valueOf(),
+                                        stats.mtime.valueOf(),
+                                        stats.ctime.valueOf()
+                                    ])
+                                )
+                            })
+                            .catch(() => undefined)
+                    ])
+                    .then((dArr) => {
+                        let s = new Set(dArr)
+                        s.delete(undefined)
+                        s.delete(NaN)
+                        let a = [...s]
+                        if (a.length != 0) return { fname: file, fdate: new Date((Math.min(...a.valueOf()))) }//fcooked
                     })
-            ])//三组日期选出最早的日期
-            .then((dArr) => {
-                console.log(dArr)
-                if (!dArr[0]) dArr.shift()
-                // if(!(dArr[0] instanceof Date)) dArr.shift()
-                return { fname: fPath, fdate: new Date((Math.min(...dArr.valueOf()))) }
+                    .catch(console.error)
+            )
+        })
+    })
+    .then(() => {
+        Promise
+            .all(pArr)
+            .then((fcooked) => {
+                exif
+                    .closeExif()//关闭exiftool
+                    .then(() => {
+                        console.log('All dates read finished\nNow start copying and classifying...\nPlease wait❤')
+                        fcooked.forEach(file => {
+                            let dest = `${file.fdate.getFullYear()}`,
+                                indest = `${file.fdate.getMonth() + 1}`
+                            fsPromises
+                                .mkdir(`${destination}/${dest}`)
+                                .catch(() => undefined)
+                                .finally(() => {
+                                    fsPromises
+                                        .mkdir(`${destination}/${dest}/${indest}`)
+                                        .catch(() => undefined)
+                                        .finally(() => {
+                                            fsPromises
+                                                .copyFile(file.fname, `${destination}/${dest}/${indest}/${path.basename(file.fname)}`)
+                                                .catch(console.error)
+                                        })
+                                })
+                        })
+                    })
+                    .catch(console.error)
             })
             .catch(console.error)
-    )
-})
-
-Promise
-    .all(pArr)
-    .then((fcooked) => {
-        exif.closeExif(openedEP)//关闭exiftool
-        console.log('files info read finished!\nnow start classifying...')
-        console.log(JSON.stringify(fcooked, null, 2))
-        fcooked.forEach(file => {
-            let dest = `${file.fdate.getFullYear()}`,
-                indest = `${file.fdate.getMonth() + 1}`
-            fsPromises
-                .mkdir(`${destination}/${dest}`)
-                .catch(() => undefined)
-                .finally(() => {
-                    fsPromises
-                        .mkdir(`${destination}/${dest}/${indest}`)
-                        .catch(() => undefined)
-                        .finally(() => {
-                            fsPromises
-                                .copyFile(file.fname, `${destination}/${dest}/${indest}/${path.basename(file.fname)}`)
-                                .catch(console.error)
-                        })
-                })
-        })
-    }, console.error)
-    .catch(console.error)
+    })
--- file-recursion.js
+++ file-recursion.js
@@ -1,26 +1,4 @@
 const fs = require('fs')
-let fileRecursion = (fileDir, cb) => {
-    let root = {},
-        slash = '/'
-    recursion(fileDir, root)
-    function recursion(fileDir, tree) {
-        let dirList = fs.readdirSync(fileDir)
-        if (dirList.length == 0) return 
-        dirList.forEach((item) => {
-            let isDir = fs.statSync(fileDir + slash + item).isDirectory(),
-                isFile = fs.statSync(fileDir + slash + item).isFile()
-            isDir && !isFile ? whenIsDir(item, tree) : whenIsFile(item, tree)
-        })
-        function whenIsDir(inItem, inTree) {
-            inTree[inItem] = {}
-            recursion(fileDir + slash + inItem, inTree[inItem])
-        }
-        function whenIsFile(inItem, inTree) {
-            cb(fileDir + slash + inItem)
-            inTree[inItem] = fileDir + slash + inItem
-        }
-    }
-}
 let fileArray = (fileDir) => {
     let root = {},
         slash = '/',
@@ -45,70 +23,4 @@ let fileArray = (fileDir) => {
     }
     return arr
 }
-
-function saveTo(srcPath, savePath) {
-    let fileRecursion = (fileDir) => {
-        let root = {},
-            slash = '/'
-        recursion(fileDir, root)
-        return root
-        function recursion(fileDir, tree) {
-            let dirList = fs.readdirSync(fileDir)
-            if (dirList.length == 0) return console.log('"' + fileDir + '"' + ' is a blank folder')
-            dirList.forEach((item) => {
-                let isDir = fs.statSync(fileDir + slash + item).isDirectory(),
-                    isFile = fs.statSync(fileDir + slash + item).isFile()
-                isDir && !isFile ? whenIsDir(item, tree) : whenIsFile(item, tree)
-            })
-            function whenIsDir(inItem, inTree) {
-                inTree[inItem] = {}
-                recursion(fileDir + slash + inItem, inTree[inItem])
-            }
-            function whenIsFile(inItem, inTree) {
-                inTree[inItem] = fileDir + slash + inItem
-            }
-        }
-    }
-    let str = fileRecursion(srcPath)
-    fs.writeFile(savePath, JSON.stringify(str, null, 2), (e) => {
-        e ? console.error(e) : console.log('文件已写入完成');
-    })
-}
-
-function list(fileDir) {
-    let fileRecursion = (fileDir) => {
-        let count = -1,
-            root = {},
-            slash = '/'
-        recursion(fileDir, root, count)
-        function recursion(fileDir, tree, n) {
-            n++
-            let dirList = fs.readdirSync(fileDir)
-            if (dirList.length == 0) return
-            dirList.forEach((item) => {
-                let isDir = fs.statSync(fileDir + slash + item).isDirectory(),
-                    isFile = fs.statSync(fileDir + slash + item).isFile()
-                isDir && !isFile ? whenIsDir(item, tree) : whenIsFile(item, tree)
-            })
-            function whenIsDir(inItem, inTree) {
-                inTree[inItem] = {}
-                console.log('+'.repeat(n) + inItem + ' :')
-                recursion(fileDir + slash + inItem, inTree[inItem], n)
-            }
-            function whenIsFile(inItem, inTree) {
-                inTree[inItem] = fileDir + slash + inItem
-                console.log('+'.repeat(n) + inItem)
-            }
-        }
-    }
-    fileRecursion(fileDir)
-}
 exports.getFilesArr = fileArray
-exports.forEachFile = fileRecursion
-exports.list = list
-exports.saveTo = saveTo
-// fileRecursion('D:/coding/pra-file/forTest', (item) => {
-//     console.log(item)
-// })
-// list('D:/coding/pra-file/forTest')
-// saveTo('D:/coding/pra-file/forTest', 'D:/coding/pra-file/saveTo.json')
